9d5d878b761616e48fd7d4ca85248d50094abe42
Pav0l <pavol.karas@gmail.com>
2019-03-05 16:37:59 +0100

    Refactored app to use higher order component

--- tick-tack-toe/src/App.jsx
+++ tick-tack-toe/src/App.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
-import PvP from './components/PlayerVsPlayer';
-import PvC from './components/PlayerVsComputer';
+import GameDom from './components/GameDom';
+import Improver from './components/PlayerVsComputer';
 import Button from './components/Button';
 import styled from 'styled-components';
 
 export default function App() {
+  const [squareArr, setSquareArr] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [isXnext, setIsXNext] = useState(true);
+
+  const resetGame = () => {
+    setSquareArr([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    setIsXNext(true);
+  }
+  
+  const emptySquares = (arrOfSquares) => {
+    return arrOfSquares.filter(sq => sq === 0);
+  }
+
+  const squareClick = (id) => {
+    const newSquareArr = squareArr.slice();
+    newSquareArr[id] = isXnext ? 'X' : 'O';
+    setSquareArr(newSquareArr);
+    setIsXNext(!isXnext);
+  }
+  
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const declareWinner = (boardArr) => {
+    for (let i = 0; i < winningLines.length; i++) {
+      const [a, b, c] = winningLines[i];
+      if (boardArr[a] && boardArr[a] === boardArr[b] && boardArr[a] === boardArr[c]) {
+        return boardArr[a] === 'X' ? 'X': 'O';
+      }
+    }
+    return null;
+  }
 
   const BtnLinks = () => {
     return (
@@ -20,6 +59,8 @@ export default function App() {
     );
   }
 
+  const PvC = Improver(GameDom);
+
   const AppComp = () => {
     return (
       <AppWrapper>
@@ -32,12 +73,35 @@ export default function App() {
 
         <Route
           path="/pvp"
-          component={PvP}
+          render={props => (
+            <GameDom
+              {...props}
+              squareArr={squareArr}
+              isXnext={isXnext}
+              squareClick={squareClick}
+              resetGame={resetGame}
+              emptySquares={emptySquares}
+              declareWinner={declareWinner}
+            />
+          )}
         />
 
         <Route
           path="/pvc"
-          component={PvC}
+          render={props => (
+            <PvC
+              {...props}
+              squareArr={squareArr}
+              setSquareArr={setSquareArr}
+              isXnext={isXnext}
+              setIsXNext={setIsXNext}
+              squareClick={squareClick}
+              resetGame={resetGame}
+              emptySquares={emptySquares}
+              declareWinner={declareWinner}
+              winningLines={winningLines}
+            />
+          )}
         />
 
       </AppWrapper>
--- tick-tack-toe/src/components/PlayerVsComputer.jsx
+++ tick-tack-toe/src/components/PlayerVsComputer.jsx
@@ -1,131 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import GameDom from './GameDom';
+import React, { useEffect } from 'react';
 
-export default function PvC() {
-  const [squareArr, setSquareArr] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  const [isXnext, setIsXNext] = useState(true);
+export default function Improver(PvPComponent) {
+  return function improvedFunc({ 
+    setIsXNext, setSquareArr, squareArr, isXnext,
+    emptySquares, declareWinner, winningLines, squareClick, resetGame
+  }) {
 
-  useEffect(() => {
-    // run computer turn on its turn and if there are some empty squares
-    if (isXnext === false && 
-      emptySquares(squareArr).length !==0 &&
-      declareWinner(squareArr) === null
-      ) {
-      setTimeout(computerTurn, 1000);
-    }
-  }, [isXnext]);
-
-  const human = 'X';
-  const computer = 'O';
-
-  // find empty squares on the game board
-  const emptySquares = (arrOfSquares) => {
-    return arrOfSquares.filter(sq => sq === 0);
-  }
-
-  // check if there is a WIN/LOSE condition (two squares has the same symbol in row/col/diag)
-  const isThereEndGameCondition = (arrOfSquares, player) => {
-    const endGameConditions = [];
-    for (let i = 0; i < winningLines.length; i++) {
-      const [a, b, c] = winningLines[i];
-      if (
-        // two squares are equal and thrid one must be empty
-        (arrOfSquares[a] === player && arrOfSquares[b] === player && arrOfSquares[c] === 0) ||
-        (arrOfSquares[a] === player && arrOfSquares[c] === player && arrOfSquares[b] === 0) ||
-        (arrOfSquares[b] === player && arrOfSquares[c] === player && arrOfSquares[a] === 0)
-      ) {
-        endGameConditions.push({
-          line: i,
-          result: [arrOfSquares[a], arrOfSquares[b], arrOfSquares[c]]
-        })
+    useEffect(() => {
+      // run computer turn on its turn and if there are some empty squares
+      if (isXnext === false && 
+        emptySquares(squareArr).length !==0 &&
+        declareWinner(squareArr) === null
+        ) {
+        computerTurn();
       }
+    }, [isXnext]);
+  
+    const human = 'X';
+    const computer = 'O';
+  
+    // check if there is a WIN/LOSE condition (two squares has the same symbol in row/col/diag)
+    const isThereEndGameCondition = (arrOfSquares, player) => {
+      const endGameConditions = [];
+      for (let i = 0; i < winningLines.length; i++) {
+        const [a, b, c] = winningLines[i];
+        if (
+          // two squares are equal and thrid one must be empty
+          (arrOfSquares[a] === player && arrOfSquares[b] === player && arrOfSquares[c] === 0) ||
+          (arrOfSquares[a] === player && arrOfSquares[c] === player && arrOfSquares[b] === 0) ||
+          (arrOfSquares[b] === player && arrOfSquares[c] === player && arrOfSquares[a] === 0)
+        ) {
+          endGameConditions.push({
+            line: i,
+            result: [arrOfSquares[a], arrOfSquares[b], arrOfSquares[c]]
+          })
+        }
+      }
+      return endGameConditions;
     }
-    return endGameConditions;
-  }
-
-  const executeEndGameCondition = (endCondition) => {
-    const newSquareArr = squareArr.slice();
-    // get line of winning/losing array
-    const endLine = endCondition[0].line;
-    // get ID inside winning line which is empty
-    const idInEndLine = endCondition[0].result.indexOf(0);
-    // get new ID where 'O' will be placed
-    const idInNewArr = winningLines[endLine][idInEndLine];
-
-    newSquareArr[idInNewArr] = 'O';
-    setSquareArr(newSquareArr);
-    setIsXNext(!isXnext);
-  }
-
-  const computerTurn = () => {
-    const winCondition = isThereEndGameCondition(squareArr, computer);
-    const loseCondition = isThereEndGameCondition(squareArr, human);
-
-    if (winCondition.length !== 0) {
-      executeEndGameCondition(winCondition);
-
-    } else if (loseCondition.length !== 0) {
-      executeEndGameCondition(loseCondition);
-
-    } else {
-      // if there is no WIN/LOSE condition, select a random empty square
-      const emptyArrIndexes = [];
+  
+    const executeEndGameCondition = (endCondition) => {
       const newSquareArr = squareArr.slice();
-      for (let i = 0; i < newSquareArr.length; i++) {
-        if (newSquareArr[i] === 0) {
-          emptyArrIndexes.push(i);
-        }        
-      }
-      const randEmptyIdx = emptyArrIndexes.length === 8 && newSquareArr[4] === 0
-        ? 4
-        : emptyArrIndexes[Math.floor(Math.random() * emptyArrIndexes.length)];
-      newSquareArr[randEmptyIdx] = 'O';
+      // get line of winning/losing array
+      const endLine = endCondition[0].line;
+      // get ID inside winning line which is empty
+      const idInEndLine = endCondition[0].result.indexOf(0);
+      // get new ID where 'O' will be placed
+      const idInNewArr = winningLines[endLine][idInEndLine];
+  
+      newSquareArr[idInNewArr] = 'O';
       setSquareArr(newSquareArr);
-      setIsXNext(!isXnext);
+      setIsXNext(true);
     }
-  }
-
-  const resetGame = () => {
-    setSquareArr([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    setIsXNext(true);
-  }
-
-  const squareClick = (id) => {
-    const newSquareArr = squareArr.slice();
-    newSquareArr[id] = isXnext ? 'X' : 'O';
-    setSquareArr(newSquareArr);
-    setIsXNext(!isXnext);
-  }
-
-  const winningLines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  const declareWinner = (boardArr) => {
-    for (let i = 0; i < winningLines.length; i++) {
-      const [a, b, c] = winningLines[i];
-      if (boardArr[a] && boardArr[a] === boardArr[b] && boardArr[a] === boardArr[c]) {
-        return boardArr[a] === 'X' ? 'X': 'O';
+  
+    const computerTurn = () => {
+      const winCondition = isThereEndGameCondition(squareArr, computer);
+      const loseCondition = isThereEndGameCondition(squareArr, human);
+  
+      if (winCondition.length !== 0) {
+        executeEndGameCondition(winCondition);
+  
+      } else if (loseCondition.length !== 0) {
+        executeEndGameCondition(loseCondition);
+  
+      } else {
+        // if there is no WIN/LOSE condition, select a random empty square
+        const emptyArrIndexes = [];
+        const newSquareArr = squareArr.slice();
+        for (let i = 0; i < newSquareArr.length; i++) {
+          if (newSquareArr[i] === 0) {
+            emptyArrIndexes.push(i);
+          }        
+        }
+        const randEmptyIdx = emptyArrIndexes.length === 8 && newSquareArr[4] === 0
+          ? 4
+          : emptyArrIndexes[Math.floor(Math.random() * emptyArrIndexes.length)];
+        newSquareArr[randEmptyIdx] = 'O';
+        setSquareArr(newSquareArr);
+        setIsXNext(true);
       }
     }
-    return null;
+    return (
+      <PvPComponent
+        setIsXNext={setIsXNext}
+        setSquareArr={setSquareArr}
+        squareArr={squareArr}
+        isXnext={isXnext}
+        emptySquares={emptySquares}
+        declareWinner={declareWinner}
+        winningLines={winningLines}
+        resetGame={resetGame}
+        squareClick={squareClick}
+      />
+    );
   }
-
-  return (
-    <GameDom
-      isXnext={isXnext}
-      squareClick={squareClick}
-      squareArr={squareArr}
-      resetGame={resetGame}
-      emptySquares={emptySquares}
-      declareWinner={declareWinner}
-    />
-  );
 }
